544c16283f2da2b0ee7d4c29a77c0a1c0f046b9f
olu-0729 <eclipsejohn07@gmail.com>
2022-12-19 23:39:53 -0800

    6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,5 @@
 g!/usr/bin/python3
 
-def raise_exception_msg(message = " " ):
+def raise_exception_msg(message=""):
     """Raise a NameError exception with a message."""
-    raise
-NameError (message)
+    raise NameError(message)
